0e549e349fa23d602375cc9196cb11d1261ac37e
Tyankaqq <Tyankaqq@outlook.com>
2025-10-28 13:01:28 +0200

    update

--- HakatonWeb/src/App.jsx
+++ HakatonWeb/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import "../src/css/style.css";
 import DashboardPage from "./pages/DashboardPage.jsx";
-import TasksPage from "./pages/TasksPage.jsx";
+
 import ExecutorsPage from "./pages/ExecutorsPage.jsx";
 import BalancerPage from "./pages/BalancerPage.jsx";
 import ParametrsPage from "./pages/ParametrsPage.jsx";
@@ -21,7 +21,6 @@ function App() {
                         <Route path="/balancer" element={<BalancerPage />} />
                         <Route path="/executors" element={<ExecutorsPage />} />
                         <Route path="/parametrs" element={<ParametrsPage />} />
-                        <Route path="/tasks" element={<TasksPage />} />
                         <Route path="/dashboard" element={<DashboardPage />} />
                         <Route path="*" element={<Navigate to="/balancer" replace />} />
                     </Routes>
